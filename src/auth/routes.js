9f484c148f59831faef4b0145407dad7dac377ca
Matt Ravenmoore <m.ravenmoore@gmail.com>
2020-12-15 21:23:19 -0800

    got stuck realy need a review of test reading and writing

---
+++ src/auth/routes.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+
+const User = require('./models/users.js');
+const basicAuth = require('./middleware/basic.js');
+const bearerAuth = require('./middleware/bearer.js');
+
+authRouter.post('/signup', async (req, res) => {
+  try {
+    let user = new User(req.body);
+    const userRecord = await user.save();
+    const output = {
+      user: userRecord,
+      token: userRecord.token,
+    };
+    res.status(201).json(output);
+  } catch (e) {(e.message);}
+});
+
+authRouter.post('/signin', basicAuth, (req, res) => {
+  const user = {
+    user: req.user,
+    token: req.user.token,
+  };
+  res.status(200).json(user);
+});
+
+authRouter.get('/users', bearerAuth, async (req, res) => {
+  const users = await User.find({});
+  const list = users.map(user => user.username);
+  res.status(200).json(list);
+});
+
+authRouter.get('/secret', bearerAuth, async (req, res) => {
+  res.status(200).send('Welcome to the secret area!');
+});
+
+
+module.exports = authRouter;
